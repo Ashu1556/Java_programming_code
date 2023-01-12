fcaa6ae923aec03f02a076a6e25dc277c8064d99
Ashu1556 <ashutoshkumar6.ak@gmail.con>
2023-01-13 03:31:53 +0530

    count_digit_number

---
+++ Countdigit/countdigit.java
@@ -0,0 +1,21 @@
+package Countdigit;
+
+import java.util.Scanner;
+
+public class countdigit {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int count = 0;
+        while (n > 0) {
+
+            n = n / 10;
+            count++;
+
+        }
+        scan.close();
+        System.out.println(count);
+    }
+
+}
